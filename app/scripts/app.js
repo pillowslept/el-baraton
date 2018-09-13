752e99ebf58274a2860e32bbc383a14de98d4a3e
Juan Camilo Velásquez Vanegas <juan.velasquez@ceiba.com.co>
2018-09-12 21:59:43 -0500

    Ui bootstrap dependency added to project.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -18,7 +18,8 @@ angular
     'ngRoute',
     'ngSanitize',
     'ngTouch',
-    'ui-notification'
+    'ui-notification',
+    'ui.bootstrap'
   ])
   .config(function ($routeProvider, NotificationProvider) {
     NotificationProvider.setOptions({
